5e3bab1817ca1cfbb4b8eca108fcedfc967d1af3
Mahesh Manore <MaheshManore048@gmail.com>
2025-08-24 18:08:20 +0530

    first commit

---
+++ src/main/java/com/backend/KaamDhundho/KaamDhundhoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.KaamDhundho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaamDhundhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaamDhundhoApplication.class, args);
+	}
+
+}
